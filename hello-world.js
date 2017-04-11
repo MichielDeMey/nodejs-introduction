2e20d82396ed362f11d8f4dc2aa5d2ea4617cf5b
Gilles De Mey <gilles.de.mey@gmail.com>
2017-04-11 20:31:59 +0200

    update hello world example

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,9 @@
-          const fs = require('fs')
+const fs = require('fs')
+const { stdout } = process
 
-          fs.readFile('world.txt', 'utf8', (err, data) => {
-            if (err) throw err
-            console.log(data)
-          })
+fs.readFile('world.txt', 'utf8', (err, data) => {
+  if (err) throw err
+  stdout.write(data)
+})
 
-          console.log('hello, ')
+stdout.write('hello, ')
